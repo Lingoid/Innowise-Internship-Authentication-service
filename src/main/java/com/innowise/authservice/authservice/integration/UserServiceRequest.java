d336527d2f53552f217fc232cdad39d67f079323
Максим Багель <maksik2000000@gmail.com>
2025-10-21 21:37:11 +0300

    feat(auth-service): Added secret word transfer to User Service

--- src/main/java/com/innowise/authservice/authservice/integration/UserServiceRequest.java
+++ src/main/java/com/innowise/authservice/authservice/integration/UserServiceRequest.java
@@ -9,16 +9,20 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class UserServiceRequest {
 
     private final WebClient webClient;
+    private final String secretWord;
 
-    public UserServiceRequest(@Value("${user.service.url}") String userServiceUrl) {
+    public UserServiceRequest(@Value("${user.service.url}") String userServiceUrl,
+                              @Value("${registration.secret-word}") String secretWord) {
         this.webClient = WebClient.builder()
                 .baseUrl(userServiceUrl)
                 .build();
+        this.secretWord = secretWord;
     }
 
     public void createUserProfile(UserDTO userDTO) {
         webClient.post()
                 .uri("")
+                .header("secret", secretWord)
                 .bodyValue(userDTO)
                 .retrieve()
                 .bodyToMono(Void.class)
